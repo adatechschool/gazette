e8869b7ae4bad6066b76ce605c4c9066f6784822
France Huon <france.huon@lonestone.io>
2025-01-10 17:35:05 +0100

    added header for pages about and home

--- apps/web/src/routes/homepage.lazy.tsx
+++ apps/web/src/routes/homepage.lazy.tsx
@@ -1,10 +1,28 @@
+import HeaderDesktopCC from '@/components/custom/HeaderDesktopCC';
+import HeaderMobileCC from '@/components/custom/HeaderMobileCC';
 import LayoutCC from '@/components/custom/LayoutCC';
+import { useBreakpointValue } from '@chakra-ui/react';
 import { createLazyFileRoute } from '@tanstack/react-router';
+import { useTranslation } from 'react-i18next';
 
 export const Route = createLazyFileRoute('/homepage')({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <LayoutCC></LayoutCC>;
+	const { t } = useTranslation('common', {
+		keyPrefix: 'navigateApp',
+	});
+
+	const isMobile = useBreakpointValue({ base: true, lg: false });
+
+	return (
+		<LayoutCC>
+			{isMobile ? (
+				<HeaderMobileCC text={t('home')} />
+			) : (
+				<HeaderDesktopCC text={t('home')} />
+			)}
+		</LayoutCC>
+	);
 }
