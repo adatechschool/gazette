64bcae4b2ee084724dfa3f693e303cf709c11520
Orianeds <oriane.dasilva@yahoo.fr>
2025-01-16 09:56:54 +0100

    feat: add parameters menu

--- apps/web/src/routes/parameters.lazy.tsx
+++ apps/web/src/routes/parameters.lazy.tsx
@@ -1,10 +1,37 @@
+import HeaderDesktopCC from '@/components/custom/HeaderDesktopCC';
+import HeaderMobileCC from '@/components/custom/HeaderMobileCC';
 import LayoutCC from '@/components/custom/LayoutCC';
+import Navbar from '@/components/custom/NavbarCC';
+import ParametersMenuCC from '@/components/custom/ParametersMenuCC';
 import { createLazyFileRoute } from '@tanstack/react-router';
+import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 
 export const Route = createLazyFileRoute('/parameters')({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <LayoutCC></LayoutCC>;
+	const { t } = useTranslation('common', {
+		keyPrefix: 'navigateApp',
+	});
+
+	const isMobile = useBreakpointValue({ base: true, lg: false });
+	return (
+		<LayoutCC>
+			{isMobile ? (
+				<Flex flexDirection='column'>
+					<HeaderMobileCC text={t('account')} />
+					<Navbar />
+					<ParametersMenuCC />
+				</Flex>
+			) : (
+				<Flex flexDirection='column'>
+					<HeaderDesktopCC text={t('account')} />
+					<ParametersMenuCC />
+				</Flex>
+			)}
+
+		</LayoutCC>
+	)
 }
