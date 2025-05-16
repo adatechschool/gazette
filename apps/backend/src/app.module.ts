fbf7c13df5ea4ad70ac2b0ad8458bc29b0b246f9
FranceHuon <francehuon@gmail.com>
2025-05-16 16:00:19 +0200

    Authentication almost done

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -6,17 +6,19 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import ormConfig from './mikro-orm.config';
+import { JwtConfigModule } from './modules/jwt/jwt.config.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    MikroOrmModule.forRoot(ormConfig),
-    UsersModule,
-    AuthModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		MikroOrmModule.forRoot(ormConfig),
+		UsersModule,
+		AuthModule,
+		JwtConfigModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- apps/web/src/routes/explore.lazy.tsx
+++ apps/web/src/routes/explore.lazy.tsx
@@ -5,9 +5,6 @@ import HeaderMobileCC from '@/components/custom/HeaderMobileCC';
 import HeaderDesktopCC from '@/components/custom/HeaderDesktopCC';
 import { useTranslation } from 'react-i18next';
 import Navbar from '@/components/custom/Navbar';
-import { getAllUsers } from '@/services/api';
-import { useEffect, useState } from 'react';
-import { CreateUserDto } from '@gazette/shared';
 
 export const Route = createLazyFileRoute('/explore')({
 	component: RouteComponent,
@@ -19,20 +16,6 @@ function RouteComponent() {
 	});
 
 	const isMobile = useBreakpointValue({ base: true, lg: false });
-	const [users, setUsers] = useState<CreateUserDto[]>([]);
-
-	useEffect(() => {
-		const fetchUsers = async () => {
-			try {
-				const usersData = await getAllUsers();
-				setUsers(usersData);
-			} catch (error) {
-				console.error('Error fetching users:', error);
-			}
-		};
-
-		fetchUsers();
-	}, []);
 
 	return (
 		<LayoutCC>
@@ -44,7 +27,6 @@ function RouteComponent() {
 			) : (
 				<div>
 					<HeaderDesktopCC text={t('explore')} />
-					<p>{users.length} users found</p>
 				</div>
 			)}
 		</LayoutCC>
