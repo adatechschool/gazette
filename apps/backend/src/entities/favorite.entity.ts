1cee8f71d45566ecfa022e35adb0caa764e92dea
Oriane Da Silva <orianedasilva@Mac.lan>
2025-06-20 11:24:04 +0200

    change primary key type

--- apps/backend/src/entities/favorite.entity.ts
+++ apps/backend/src/entities/favorite.entity.ts
@@ -1,11 +1,11 @@
-import { Entity, ManyToOne, Property } from '@mikro-orm/core'
+import { Entity, ManyToOne, PrimaryKey, Property } from '@mikro-orm/core'
 import { PrimaryKeyUuid } from 'src/utils/PrimaryKeyUuid.decorator'
 import { Media } from './media.entity'
 import { User } from './user.entity'
 
 @Entity()
 export class Favorite {
-  @PrimaryKeyUuid()
+  @PrimaryKey({type:'uuid', defaultRaw:'gen_random_uuid()'})
   id!: string
 
   @ManyToOne(() => User)
