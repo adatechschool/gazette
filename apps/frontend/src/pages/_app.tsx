cb084bc76ae38b6770d7cab4871fcc501c48ae5e
FranceHuon <francehuon@gmail.com>
2025-06-25 22:00:53 +0200

    Wrap app with AuthProvider

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { I18nextProvider } from 'react-i18next'
 import { Provider } from '@/components/ui/provider'
+import { AuthProvider } from '@/contexts/AuthContext'
 import Layout from '../components/custom/Layout'
 import i18n from '../i18n/config'
 
@@ -13,9 +14,11 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <I18nextProvider i18n={i18n}>
         <Provider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+          <AuthProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </AuthProvider>
         </Provider>
       </I18nextProvider>
     </>
