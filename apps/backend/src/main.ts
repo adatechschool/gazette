c712159248031179ddd94e4ae00f872127adc1b6
elinelgp <elinelgp@gmail.com>
2025-07-26 23:47:32 +0200

    fix: remove extra semicolon

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
 import { Logger } from 'nestjs-pino'
 import { AppModule } from './app.module'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -14,15 +14,15 @@ async function bootstrap() {
       allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With, Origin',
       exposedHeaders: 'Set-Cookie',
     },
-  });
+  })
   const config = new DocumentBuilder()
     .setTitle('Gazette API')
     .setDescription('API pour l\'application Gazette')
     .setVersion('1.0')
     .addTag('gazette')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api-docs', app, document)
 
   app.useLogger(app.get(Logger))
   app.use(cookieParser())
