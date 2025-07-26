fd7b04a16a4b231bc30c28582616be0210b3d3ae
elinelgp <elinelgp@gmail.com>
2025-07-26 23:54:32 +0200

    fix: imports sort

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
 import { Logger } from 'nestjs-pino'
-import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
