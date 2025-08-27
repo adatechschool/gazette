b4ca3d263948e3fe343a5cb202884e45a6e652f6
elinelgp <elinelgp@gmail.com>
2025-08-27 17:18:25 +0200

    fix: forms

--- apps/backend/src/modules/auth/auth.service.ts
+++ apps/backend/src/modules/auth/auth.service.ts
@@ -25,7 +25,7 @@ export class AuthService {
         throw new UnauthorizedException('Invalid credentials')
       }
 
-      const payload = { id: user.id, email: user.email, pseudo: user.pseudo }
+      const payload = { id: user.id }
       return {
         access_token: await this.jwtService.signAsync(payload, {
           secret: this.configService.get('JWT_SECRET'),
